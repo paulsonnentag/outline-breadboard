395cef7dcb3c94c9f0f4fdcf4b49ccdd0a5e7679
Paul Sonnentag <paul.sonnentag@googlemail.com>
2023-05-12 14:43:03 +0200

    Show sunset/sunrise as suggested functions

--- src/language/functions/sunset.tsx
+++ src/language/functions/sunset.tsx
@@ -1,14 +1,53 @@
-import { FunctionDefs } from "./function-def"
+import { FunctionDef, FunctionDefs } from "./function-def"
 import { parseDate, parseLatLng } from "../../properties"
 import SunCalc from "suncalc"
+import { FunctionSuggestion, Parameter } from "../function-suggestions"
 
-// TODO: doesn't do tree traversal
 export const SUNSET_FN: FunctionDefs = {
-  Sunset: {
+  Sunset: getSolarFunctionDef("sunset"),
+  Sunrise: getSolarFunctionDef("sunrise"),
+}
+
+function getSolarFunctionDef(property: "sunrise" | "sunset"): FunctionDef {
+  const name = property === "sunrise" ? "Sunrise" : "Sunset"
+  const emoji = property === "sunrise" ? "🌅" : "🌌"
+
+  return {
     icon: "wb_twilight",
+    summaryView: (value) => `${emoji} ${value}`,
+    suggestions: (parameters: Parameter[]) => {
+      const dates = parameters.filter((p) => p.value.type === "date")
+      const locations = parameters.filter((p) => p.value.type === "location")
+
+      const suggestions: FunctionSuggestion[] = []
+
+      for (const date of dates) {
+        for (const location of locations) {
+          let rank = location.distance + date.distance
+
+          suggestions.push({
+            icon: "wb_twilight",
+            name,
+            arguments: [
+              {
+                label: "in",
+                value: location.value.expression,
+              },
+              {
+                label: "on",
+                value: date.value.expression,
+              },
+            ],
+            rank,
+          })
+        }
+      }
+
+      return suggestions
+    },
     autocomplete: {
       icon: "wb_twilight",
-      name: "Sunset",
+      name,
       arguments: [
         {
           label: "in",
@@ -30,9 +69,13 @@ export const SUNSET_FN: FunctionDefs = {
         }
 
         const times = SunCalc.getTimes(onDate, 51.5, -0.1)
+        const value: Date = times[property]
 
-        return `${times.sunrise.getHours()}:${times.sunrise.getMinutes()}`
+        return `${value.getHours().toString().padStart(2, "0")}:${value
+          .getMinutes()
+          .toString()
+          .padStart(2, "0")}`
       }
     },
-  },
+  }
 }

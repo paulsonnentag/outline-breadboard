288c1c657f3dca8e458806e47ecf5f4f0e05a176
Paul Sonnentag <paul.sonnentag@googlemail.com>
2024-01-12 17:14:26 +0100

    Add trip example

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,11 +1,13 @@
 import seattle36Hours from "./seattle_36_hours.json"
 import polesOfInconvenience from "./poles_of_inconvenience.json"
 import functionOverview from "./function_overview.json"
+import friendsVisiting from "./friends_visiting.json"
 import detailPlanPoi16 from "./detail_plan_poi_16.json"
 import { GraphDoc } from "../graph"
 
 export const EXAMPLES: GraphDoc[] = [
-  functionOverview as GraphDoc,
-  seattle36Hours as GraphDoc,
+  friendsVisiting as GraphDoc,
   polesOfInconvenience as GraphDoc,
+  seattle36Hours as GraphDoc,
+  functionOverview as GraphDoc,
 ]
